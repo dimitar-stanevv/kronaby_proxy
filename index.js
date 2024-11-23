6cf1829a5aca5f9381f0744ac905030d3b37e0d7
Dimitar Stanev <dimitar.stanev@icloud.com>
2024-11-23 22:19:28 +0200

    Add hostname

--- index.js
+++ index.js
@@ -151,4 +151,4 @@ app.get("/gigacharger/start", async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.listen(3000, "0.0.0.0", () => console.log('Server running on http://localhost:3000'));
